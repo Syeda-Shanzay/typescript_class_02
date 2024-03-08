393ee23a19a5b58099cd8c77c9f7e89ef071285c
Syeda-Shanzay <syedashanzay04@gmail.com>
2024-03-09 04:34:20 +0500

    first commit

---
+++ variable.js
@@ -0,0 +1,46 @@
+// Variable Declaration
+// Method#1
+var user_name = "Syeda";
+console.log(user_name);
+// Method#2
+var myage;
+console.log(myage);
+// Method#3
+var myvariable = "abc";
+console.log(myvariable);
+// Method#4
+var myaddress;
+console.log(myaddress);
+// Concatenate
+var uname = "Shanzay";
+var ulocation = "Karachi";
+var marks = 500;
+var percentage = 80;
+console.log("name:", uname);
+console.log("location:", ulocation);
+console.log("marks:", marks);
+console.log("percentage:", percentage);
+// Let_vs_Const
+var currentbalance = 25000; // This value can change
+currentbalance = 20000;
+console.log(currentbalance);
+var accountnumber = 123456789; // This value remain const
+console.log(accountnumber);
+// Var
+var myname = "Syeda";
+console.log(myname);
+var myname = "Syeda";
+console.log(myname);
+var myname = "Ali";
+console.log(myname);
+// Let
+var a = "syeda";
+console.log(a);
+var b = "syeda";
+console.log(b);
+var c = "syeda";
+c = "Ali";
+console.log(c);
+// Const
+var PI = 3.14121;
+console.log(PI);
---
+++ variable.ts
@@ -0,0 +1,57 @@
+// Variable Declaration
+
+// Method#1
+var user_name : string = "Syeda"
+console.log(user_name)
+
+// Method#2
+// var myage : string;
+// console.log(myage)
+
+// Method#3
+var myvariable = "abc"
+console.log(myvariable)
+
+// Method#4
+var myaddress
+console.log(myaddress)
+
+// Concatenate
+
+var uname = "Shanzay"
+var ulocation : string = "Karachi"
+var marks = 500
+var percentage : number = 80
+
+console.log("name:" , uname)
+console.log("location:" , ulocation)
+console.log("marks:" , marks)
+console.log("percentage:" , percentage)
+
+// Let_vs_Const
+let currentbalance = 25000 // This value can change
+currentbalance = 20000
+console.log(currentbalance)
+const accountnumber = 123456789  // This value remain const
+console.log(accountnumber)
+
+// Var
+var myname = "Syeda"
+console.log(myname)
+var myname = "Syeda"
+console.log(myname)
+var myname = "Ali"
+console.log(myname)
+
+// Let
+let a = "syeda"
+console.log(a)
+let b = "syeda"
+console.log(b)
+let c = "syeda"
+c = "Ali"
+console.log(c)
+
+// Const
+const PI : number = 3.14121
+console.log(PI)
